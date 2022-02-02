370bb46b1177b496829b60bff7b5714883a6b51d
kirkwood616 <kirkwood616@gmail.com>
2022-02-02 09:55:47 -0500

    Initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//ROUTES
+// app.use("/", shoutRoutes);
+
+export const api = functions.https.onRequest(app);
